b79743e10eda860f49f8addb2d04f4ea55002c5b
honey1b <nh822@naver.com>
2020-08-27 00:17:44 +0900

    using annotation @Autowired @Resource

---
+++ spring_test03/src/main/java/com/word/service/WordRegisterServiceAutowired.java
@@ -0,0 +1,44 @@
+package com.word.service;
+
+import javax.annotation.Resource;
+
+import com.word.WordSet;
+import com.word.dao.WordDao;
+
+public class WordRegisterServiceAutowired {
+
+	
+//	@Qualifier("usedDao")
+//	@Autowired
+	@Resource
+	private WordDao wordDao;
+	
+	public WordRegisterServiceAutowired() {
+		// TODO Auto-generated constructor stub
+	}
+	
+//	@Autowired
+	public WordRegisterServiceAutowired(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+	public void register(WordSet wordSet) {
+		String wordKey = wordSet.getWordKey();
+		if(verify(wordKey)) {
+			wordDao.insert(wordSet);
+		} else {
+			System.out.println("The word has already registered.");
+		}
+	}
+	
+	public boolean verify(String wordKey){
+		WordSet wordSet = wordDao.select(wordKey);
+		return wordSet == null ? true : false;
+	}
+	
+//	@Autowired
+	public void setWordDao(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+}
