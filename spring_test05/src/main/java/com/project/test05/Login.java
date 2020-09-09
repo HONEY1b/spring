7310621a6c70bcea9c011fa308f9805922c8c66c
honey1b <nh822@naver.com>
2020-09-09 17:30:47 +0900

    using mvc

---
+++ spring_test05/src/main/java/com/project/test05/Login.java
@@ -0,0 +1,16 @@
+package com.project.test05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Login {
+
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("loginKey","loginValue");
+		return "login";//login.jsp
+	}
+}
